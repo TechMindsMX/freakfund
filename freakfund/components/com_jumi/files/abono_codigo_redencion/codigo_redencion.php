30baffa64af8ea6f26015f25b60d96c9818bbccc
erikjbb <erikjbb@gmail.com>
2013-09-23 18:23:33 -0500

    forms estaod de cuenta y codigo

---
+++ freakfund/components/com_jumi/files/abono_codigo_redencion/codigo_redencion.php
@@ -0,0 +1,48 @@
+<?php 
+
+defined('_JEXEC') OR defined('_VALID_MOS') OR die( "Direct Access Is Not Allowed" );
+$usuario = JFactory::getUser();
+$app = JFactory::getApplication();
+if ($usuario->guest == 1) {
+	$return = JURI::getInstance()->toString();
+	$url    = 'index.php?option=com_users&view=login';
+	$url   .= '&return='.base64_encode($return);
+	$app->redirect($url, JText::_('JGLOBAL_YOU_MUST_LOGIN_FIRST'), 'message');
+}
+
+jimport('trama.class');
+require_once 'components/com_jumi/files/crear_proyecto/classIncludes/libreriasPP.php';
+
+//si proyid no esta vacio traigo los datos del Producto del servicio del middleware
+$token = JTrama::token();
+
+$input = JFactory::getApplication()->input;
+$usuario= JFactory::getUser();
+$datosUsuario=JTrama::getUserBalance($usuario->id);
+
+//definicion de campos del formulario
+$action = '#';
+//$action = 'components/com_jumi/files/costos_variables/post.php';
+?>
+
+<script>
+	jQuery(document).ready(function(){
+		jQuery("#form_codigo").validationEngine();
+		
+		
+	});
+</script>
+<h1><?php echo JText::_('REDENCION_CODIGO');  ?></h1>
+<div>
+	<form id="form_codigo" action="<?php echo $action; ?>" method="POST">
+	
+		<?php echo JText::_('CODIGO_PROMO');  ?> <input type="text" name="fname">
+		
+		<div style="margin: 10px;">
+			<input type="button" class="button" value="<?php echo JText::_('CANCELAR');  ?>" onClick="if(confirm('<?php echo JText::_('CONFIRMAR_CANCELAR');  ?>'))
+		javascript:window.history.back();">
+			<input type="button" class="button" value="Consultar" />
+		</div>
+	</form>
+	
+</div>
---
+++ freakfund/components/com_jumi/files/estado_cuenta/estado_cuenta.php
@@ -0,0 +1,55 @@
+<?php 
+
+defined('_JEXEC') OR defined('_VALID_MOS') OR die( "Direct Access Is Not Allowed" );
+$usuario = JFactory::getUser();
+$app = JFactory::getApplication();
+if ($usuario->guest == 1) {
+	$return = JURI::getInstance()->toString();
+	$url    = 'index.php?option=com_users&view=login';
+	$url   .= '&return='.base64_encode($return);
+	$app->redirect($url, JText::_('JGLOBAL_YOU_MUST_LOGIN_FIRST'), 'message');
+}
+
+jimport('trama.class');
+require_once 'components/com_jumi/files/crear_proyecto/classIncludes/libreriasPP.php';
+
+//si proyid no esta vacio traigo los datos del Producto del servicio del middleware
+$token = JTrama::token();
+
+$input = JFactory::getApplication()->input;
+$usuario= JFactory::getUser();
+$datosUsuario=JTrama::getUserBalance($usuario->id);
+
+//definicion de campos del formulario
+$action = '#';
+//$action = 'components/com_jumi/files/costos_variables/post.php';
+?>
+
+<script>
+	jQuery(document).ready(function(){
+		jQuery("#form_cuenta").validationEngine();
+		
+		
+	});
+</script>
+<h1><?php echo JText::_('ESTADO_CUENTA');  ?></h1>
+<div>
+	<form id="form_cuenta" action="<?php echo $action; ?>" method="POST">
+	
+		<select name="fechas" >
+			  <option value="Enero">Enero</option>
+			  <option value="Febrero">Febrero</option>
+			  <option value="Marzo">Marzo</option>
+			  <option value="Abril">Abril</option>
+		</select>
+		
+		<?php echo JText::_('RANGO_FECHA_INICIO');  ?> <input type="text" name="fechaini"><br>
+  		<?php echo JText::_('RANGO_FECHA_FIN');  ?> <input type="text" name="fechafin"><br>
+		<div style="margin: 10px;">
+			<input type="button" class="button" value="<?php echo JText::_('CANCELAR');  ?>" onClick="if(confirm('<?php echo JText::_('CONFIRMAR_CANCELAR');  ?>'))
+		javascript:window.history.back();">
+			<input type="button" class="button" value="Consultar" />
+		</div>
+	</form>
+	
+</div>
