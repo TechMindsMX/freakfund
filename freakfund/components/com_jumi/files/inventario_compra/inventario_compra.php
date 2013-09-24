32b9b4bdce82a80a1cee1519594455de13b2063d
erikjbb <erikjbb@gmail.com>
2013-09-24 11:41:49 -0500

    correcciones

--- freakfund/components/com_jumi/files/inventario_compra/inventario_compra.php
+++ freakfund/components/com_jumi/files/inventario_compra/inventario_compra.php
@@ -11,7 +11,6 @@ if ($usuario->guest == 1) {
 }
 
 jimport('trama.class');
-require_once 'components/com_jumi/files/crear_proyecto/classIncludes/libreriasPP.php';
 
 //si proyid no esta vacio traigo los datos del Producto del servicio del middleware
 $token = JTrama::token();
