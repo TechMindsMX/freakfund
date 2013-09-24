55ce158918a61a24e1d0d236dae3ad41a183692c
lutek <lutek.valhalla@gmail.com>
2013-09-24 09:07:26 -0500

    componente liquidacion a productor

--- freakfund/administrator/components/com_freakfund/models/liquidacionprod.php
+++ freakfund/administrator/components/com_freakfund/models/liquidacionprod.php
@@ -3,6 +3,7 @@
 defined('_JEXEC') or die('Restricted access');
 // import the Joomla modellist library
 jimport('joomla.application.component.modellist');
+jimport('trama.class');
 
 class liquidacionprodModelliquidacionprod extends JModelList
 {
@@ -10,8 +11,10 @@ class liquidacionprodModelliquidacionprod extends JModelList
         	$temporal = JFactory::getApplication()->input;
 			$temporal = $temporal->get('id');
 			
-        	$query = json_decode(file_get_contents(MIDDLE.PUERTO.'/trama-middleware/rest/project/get/'.$temporal));
-			$query->CRE = '4000000';
+			$query = JTrama::getDatos($temporal);
+        	// $query = json_decode(file_get_contents(MIDDLE.PUERTO.'/trama-middleware/rest/project/get/'.$temporal));
+			$query->token = JTrama::token();
+			$query->callback = JURI::base().'index.php?option=com_freakfund';
 			$query->CPR = '2000000';
 			
 			return $query;
--- freakfund/administrator/components/com_freakfund/views/liquidacionprod/tmpl/default_body.php
+++ freakfund/administrator/components/com_freakfund/views/liquidacionprod/tmpl/default_body.php
@@ -11,12 +11,16 @@ $datos = $this->items;
 	<tr>
 		<td align="absmiddle">
 			<label for="montoPago"><?php echo JText::_('COM_FREAKFUND_LIQUIDACIONPROD_BODY_MONTOPAGO'); ?></label>
-			<input type="text" name="montoPago" id="montoPago" />
+			<input type="hidden" name="token" id="token" value="<?php echo $datos->token; ?>" />
+			<input type="hidden" name="prodId" id="prodId" value="<?php echo $datos->id; ?>" />
+			<input type="hidden" name="userId" id="userId" value="<?php echo $datos->userId; ?>" />
+			<input type="hidden" name="callback" id="callback" value="<?php echo $datos->callback; ?>" />
+			<input type="text" name="payment" id="payment" />
 		</td>
 	</tr>
 	<tr>
 		<td align="absmiddle">
-			<?php echo JText::_('COM_FREAKFUND_LIQUIDACIONPROD_BODY_SALDOPROD').': $<span class="number">'.($datos->CRE-$datos->CPR).'</span>'; ?>
+			<?php echo JText::_('COM_FREAKFUND_LIQUIDACIONPROD_BODY_SALDOPROD').': $<span class="number">'.($datos->CPR-$datos->cre).'</span>'; ?>
 		</td>
 	</tr>
 	<tr>
@@ -27,6 +31,6 @@ $datos = $this->items;
 	
 	<tr>
 		<td align="absmiddle">
-			<input type="button" value="<?php echo JText::_('COM_FREAKFUND_LIQUIDACIONPROD_BODY_SEND'); ?>" />
+			<input type="submit" value="<?php echo JText::_('COM_FREAKFUND_LIQUIDACIONPROD_BODY_SEND'); ?>" />
 		</td>
 	</tr>
