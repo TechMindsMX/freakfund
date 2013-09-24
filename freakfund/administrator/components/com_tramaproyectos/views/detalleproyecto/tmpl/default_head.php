1248a5bf98200d232468044108dc7a9611eb983d
Ricardo <ricardolyon@gmail.com>
2013-09-24 10:57:59 -0500

    se eliminan componentes trama_gremios y trama_proyectos

--- freakfund/administrator/components/com_tramaproyectos/views/detalleproyecto/tmpl/default_head.php
+++
@@ -1,9 +0,0 @@
-<?php
-// No direct access to this file
-defined('_JEXEC') or die('Restricted Access');
-?>
-<tr>
-        <th width="5">
-            <?php echo JText::_('COM_TRAMAPROYECTOS_TRAMAPROYECTOS_HEADING_DETALLE'); ?>
-        </th>
-</tr>
