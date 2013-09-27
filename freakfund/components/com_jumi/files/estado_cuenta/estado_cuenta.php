79f5846eadf8b82a0a7bc9d256b0432f0086968b
erikjbb <erikjbb@gmail.com>
2013-09-27 11:32:06 -0500

    estado de cuenta con filtro y datos simulados

--- freakfund/components/com_jumi/files/estado_cuenta/estado_cuenta.php
+++ freakfund/components/com_jumi/files/estado_cuenta/estado_cuenta.php
@@ -16,40 +16,137 @@ require_once 'components/com_jumi/files/classIncludes/libreriasPP.php';
 //si proyid no esta vacio traigo los datos del Producto del servicio del middleware
 $token = JTrama::token();
 
-$input = JFactory::getApplication()->input;
-$usuario= JFactory::getUser();
 $datosUsuario=JTrama::getUserBalance($usuario->id);
 
 //definicion de campos del formulario
 $action = '#';
 //$action = 'components/com_jumi/files/costos_variables/post.php';
+
+include_once ("objetote.php");
+
+$tableHtml = "<table id='edocta_table' border='1'>";
+$tableHtml .= "<tr id='cabezera'>";
+$tableHtml .= "<th>Fecha<th />";
+$tableHtml .= "<th>Movimiento<th />";
+$tableHtml .= "<th>Tipo<th />";
+$tableHtml .= "<th>Detalle<th />";
+$tableHtml .= "<th>Referencia<th />";
+$tableHtml .= "<th>Insitucion<th />";
+$tableHtml .= "<th>Tasa<th />";
+$tableHtml .= "<th>Cantidad<th />";
+$tableHtml .= "<th>Unitario<th />";
+$tableHtml .= "<th>Subtotal<th />";
+$tableHtml .= "<th>Saldo<th />";
+$tableHtml .= "<th>Notas<th />";
+$tableHtml .= "</tr>";
+
+$selectTipo = '<select name="tipo" id="filtroTipo">';
+$selectTipo .=	"<option value='nada' selected>Sin Filtro</option>";
+
+foreach ($arregloobjetos as $obj) {
+	$tableHtml .= "<tr id='".$obj->tipo ."'>";
+	$tableHtml .= "<td>" .$obj->fecha . "<td />";
+	$tableHtml .= "<td>" .$obj->movimiento . "<td />";
+	$tableHtml .= "<td>" .$obj->tipo . "<td />";
+	$tableHtml .= "<td>" .$obj->detalle . "<td />";
+	$tableHtml .= "<td>" .$obj->referencia . "<td />";
+	$tableHtml .= "<td>" .$obj->institucion . "<td />";
+	$tableHtml .= "<td>" .$obj->tasa . "<td />";
+	$tableHtml .= "<td>" .$obj->cantidad . "<td />";
+	$tableHtml .= "<td>" .$obj->unitario . "<td />";
+	$tableHtml .= "<td>" .$obj->subtotal . "<td />";
+	$tableHtml .= "<td>" .$obj->saldo . "<td />";
+	$tableHtml .= "<td>" .$obj->notas . "<td />";
+	$tableHtml .= "</tr>";
+
+	
+}
+
+foreach ($arregloCatalogo as $obj) {
+	$selectTipo .=	"<option value='" .$obj->name . "'>" .$obj->name . "</option>";
+}
+
+$tableHtml .= "</table>";
+$selectTipo .='</select>';
 ?>
 
 <script>
 	jQuery(document).ready(function(){
 		jQuery("#form_cuenta").validationEngine();
 		
-		
+		jQuery('#filtroTipo').change(function(){
+			var limite = jQuery('#edocta_table tr').length;
+
+			if(this.value == 'nada'){
+			    jQuery('#edocta_table tr').show();
+			}else{
+			    var variable=this.value;			    
+console.log(variable);
+	           	jQuery('#edocta_table tr').hide();
+		       	jQuery('#edocta_table tr#'+variable).show();	
+		    	jQuery('#edocta_table tr#cabezera').show();		    
+			}	
+		});
 	});
 </script>
 <h1><?php echo JText::_('ESTADO_CUENTA');  ?></h1>
 <div>
 	<form id="form_cuenta" action="<?php echo $action; ?>" method="POST">
 	
+	
 		<select name="fechas" >
 			  <option value="Enero">Enero</option>
 			  <option value="Febrero">Febrero</option>
 			  <option value="Marzo">Marzo</option>
 			  <option value="Abril">Abril</option>
+			  <option value="Mayo">Mayo</option>
+			 
 		</select>
+					
+		<?php echo JText::_('RANGO_FECHA_INICIO');  ?> <input type="text" name="fechaini">
+  		<?php echo JText::_('RANGO_FECHA_FIN');  ?> <input type="text" name="fechafin">
 		
-		<?php echo JText::_('RANGO_FECHA_INICIO');  ?> <input type="text" name="fechaini"><br>
-  		<?php echo JText::_('RANGO_FECHA_FIN');  ?> <input type="text" name="fechafin"><br>
-		<div style="margin: 10px;">
-			<input type="button" class="button" value="<?php echo JText::_('CANCELAR');  ?>" onClick="if(confirm('<?php echo JText::_('CONFIRMAR_CANCELAR');  ?>'))
-		javascript:window.history.back();">
-			<input type="button" class="button" value="Consultar" />
-		</div>
+		<input type="button" class="button" value="Consultar" />
+		
 	</form>
+		<?php echo $selectTipo; ?>
+		<div>
+		
+			<table border="1">
+				<tr>
+					<td><?php echo JText::_('SOCIO');?></td>
+					<td><?php echo $usuario->name;?></td>
+				</tr>
+				<tr>
+					<td><?php echo JText::_('SALDO');?></td>
+					<td><?php echo $datosUsuario->balance;?></td>
+				</tr>
+				<tr>
+					<td>Financiamientos</td>
+					<td>1,700</td>
+				</tr>
+				<tr>
+					<td>Inversiones</td>
+					<td>150</td>
+				</tr>
+				<tr>
+					<td>Valor de cartera</td>
+					<td>1850</td>
+				</tr>
+				<tr>
+					<td>Retornos acumulados</td>
+					<td>374</td>
+				</tr>
+				<tr>
+					<td>Utilidad acumulada</td>
+					<td>161</td>
+				</tr>
+			</table>
+		</div>
+		
+		<div style="margin-top:20px;">
+		<?php echo $tableHtml; ?>
+		</div>
+		
 	
 </div>
---
+++ freakfund/components/com_jumi/files/estado_cuenta/objetote.php
@@ -0,0 +1,410 @@
+<?php
+$arregloobjetos = Array();
+
+
+$obj = new stdClass;
+$obj->fecha = '02-ene';
+$obj->movimiento = 'abono';
+$obj->tipo = 'deposito';
+$obj->detalle = 'efectivo';
+$obj->referencia = '65465461';
+$obj->institucion = 'hsbc';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '1000.00';
+$obj->subtotal = '1000.00';
+$obj->saldo = '1000.00';
+$obj->notas = '';
+$arregloobjetos[0] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '04-ene';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'transferencia';
+$obj->detalle = 'donativos';
+$obj->referencia = '65498428524';
+$obj->institucion = 'teleton';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-300.00';
+$obj->subtotal = '-300.00';
+$obj->saldo = '700.00';
+$obj->notas = '';
+$arregloobjetos[1] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '5-ene';
+$obj->movimiento = 'abono';
+$obj->tipo = 'deposito';
+$obj->detalle = 'transferencia bancaria';
+$obj->referencia = '5678687';
+$obj->institucion = 'banorte';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '50.00';
+$obj->subtotal = '50.00';
+$obj->saldo = '750.00';
+$obj->notas = '';
+$arregloobjetos[2] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '10-ene';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'inversion';
+$obj->detalle = 'en productos';
+$obj->referencia = '587687';
+$obj->institucion = 'fonca';
+$obj->tasa = '';
+$obj->cantidad = '2';
+$obj->unitario = '120.00';
+$obj->subtotal = '-240.00';
+$obj->saldo = '510.00';
+$obj->notas = '';
+$arregloobjetos[3] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '16-ene';
+$obj->movimiento = 'abono';
+$obj->tipo = 'redencion';
+$obj->detalle = 'promociones';
+$obj->referencia = '576786578';
+$obj->institucion = 'casa ley';
+$obj->tasa = '';
+$obj->cantidad = '1';
+$obj->unitario = '134.00';
+$obj->subtotal = '134.00';
+$obj->saldo = '644.00';
+$obj->notas = '';
+$arregloobjetos[4] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '17-ene';
+$obj->movimiento = 'abono';
+$obj->tipo = 'deposito';
+$obj->detalle = 'paypal';
+$obj->referencia = '26377683';
+$obj->institucion = '';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '150.00';
+$obj->subtotal = '150.00';
+$obj->saldo = '907.00';
+$obj->notas = '';
+$arregloobjetos[5] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '19-ene';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'retiros';
+$obj->detalle = 'en efectivo';
+$obj->referencia = '6546546';
+$obj->institucion = 'cajero ixe';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-450.00';
+$obj->subtotal = '-450.00';
+$obj->saldo = '594.00';
+$obj->notas = '';
+$arregloobjetos[6] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '27-ene';
+$obj->movimiento = 'abono';
+$obj->tipo = 'redencion';
+$obj->detalle = 'cupones';
+$obj->referencia = '6576786187';
+$obj->institucion = 'comerial mexicana';
+$obj->tasa = '';
+$obj->cantidad = '1';
+$obj->unitario = '163.00';
+$obj->subtotal = '163.00';
+$obj->saldo = '757.00';
+$obj->notas = '';
+$arregloobjetos[7] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '28-ene';
+$obj->movimiento = 'abono';
+$obj->tipo = 'deposito';
+$obj->detalle = 'amazon';
+$obj->referencia = '6345645';
+$obj->institucion = 'banorte';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '150.00';
+$obj->subtotal = '150.00';
+$obj->saldo = '907.00';
+$obj->notas = '';
+$arregloobjetos[8] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '2-feb';
+$obj->movimiento = 'abono';
+$obj->tipo = 'deposito';
+$obj->detalle = 'tdc';
+$obj->referencia = '786287';
+$obj->institucion = 'mastercard';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '1200.00';
+$obj->subtotal = '1200.00';
+$obj->saldo = '1407.00';
+$obj->notas = '';
+$arregloobjetos[9] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '2-feb';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'retiros';
+$obj->detalle = 'transferencia a terceros';
+$obj->referencia = '275345345';
+$obj->institucion = 'luneta';
+$obj->tasa = '';
+$obj->cantidad = '4';
+$obj->unitario = '250.00';
+$obj->subtotal = '-1000.00';
+$obj->saldo = '407.00';
+$obj->notas = '';
+$arregloobjetos[10] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '4-feb';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'retiros';
+$obj->detalle = 'transferencia a terceros';
+$obj->referencia = '5767';
+$obj->institucion = 'paypal';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-50.00';
+$obj->subtotal = '-50.00';
+$obj->saldo = '357.00';
+$obj->notas = '';
+$arregloobjetos[11] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '6-feb';
+$obj->movimiento = 'abono';
+$obj->tipo = 'deposito';
+$obj->detalle = 'tdd';
+$obj->referencia = '57267';
+$obj->institucion = 'visa';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '400.00';
+$obj->subtotal = '400.00';
+$obj->saldo = '757.00';
+$obj->notas = '';
+$arregloobjetos[12] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '8-feb';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'retiros';
+$obj->detalle = 'tdd';
+$obj->referencia = '6278';
+$obj->institucion = 'visa';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-123.00';
+$obj->subtotal = '-123.00';
+$obj->saldo = '634.00';
+$obj->notas = '';
+$arregloobjetos[13] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '12-feb';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'inversion';
+$obj->detalle = 'productos';
+$obj->referencia = '62857678';
+$obj->institucion = 'cafe tacuva';
+$obj->tasa = '';
+$obj->cantidad = '1';
+$obj->unitario = '150';
+$obj->subtotal = '-150.00';
+$obj->saldo = '484.00';
+$obj->notas = '';
+$arregloobjetos[14] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '14-feb';
+$obj->movimiento = 'abono';
+$obj->tipo = 'redencion';
+$obj->detalle = 'tarjetas regalo';
+$obj->referencia = '5276868';
+$obj->institucion = 'itunes';
+$obj->tasa = '';
+$obj->cantidad = '1';
+$obj->unitario = '200.00';
+$obj->subtotal = '200.00';
+$obj->saldo = '684.00';
+$obj->notas = '';
+$arregloobjetos[15] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '17-feb';
+$obj->movimiento = 'cargos';
+$obj->tipo = 'retiros';
+$obj->detalle = 'spei';
+$obj->referencia = '63546345';
+$obj->institucion = 'banorte';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-300.00';
+$obj->subtotal = '-300.00';
+$obj->saldo = '384.00';
+$obj->notas = '';
+$arregloobjetos[16] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '17-feb';
+$obj->movimiento = 'cargos';
+$obj->tipo = 'comisiones';
+$obj->detalle = 'bancarias';
+$obj->referencia = '63546345';
+$obj->institucion = 'banorte';
+$obj->tasa = '';
+$obj->cantidad = '1';
+$obj->unitario = '-50.00';
+$obj->subtotal = '-50.00';
+$obj->saldo = '357.00';
+$obj->notas = '';
+$arregloobjetos[17] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '17-feb';
+$obj->movimiento = 'cargos';
+$obj->tipo = 'impuestos';
+$obj->detalle = 'iva';
+$obj->referencia = '63546345';
+$obj->institucion = 'banorte';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '400.00';
+$obj->subtotal = '400.00';
+$obj->saldo = '757.00';
+$obj->notas = '';
+$arregloobjetos[18] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '20-feb';
+$obj->movimiento = 'abono';
+$obj->tipo = 'reembolso';
+$obj->detalle = 'por cancelacion';
+$obj->referencia = '654645';
+$obj->institucion = 'fonca';
+$obj->tasa = '';
+$obj->cantidad = '2';
+$obj->unitario = '-120.00';
+$obj->subtotal = '240.00';
+$obj->saldo = '566.00';
+$obj->notas = '';
+$arregloobjetos[19] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '22-feb';
+$obj->movimiento = 'cargo';
+$obj->tipo = 'transferencia';
+$obj->detalle = 'socios terceros';
+$obj->referencia = '63546345';
+$obj->institucion = 'grumpy cat';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-100.00';
+$obj->subtotal = '-100.00';
+$obj->saldo = '466.00';
+$obj->notas = '';
+$arregloobjetos[20] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '23-feb';
+$obj->movimiento = 'abono';
+$obj->tipo = 'transferencia';
+$obj->detalle = 'solcios terceros';
+$obj->referencia = '63546345';
+$obj->institucion = 'grumpy cat';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '50.00';
+$obj->subtotal = '50.00';
+$obj->saldo = '516.00';
+$obj->notas = '';
+$arregloobjetos[21] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '25-feb';
+$obj->movimiento = 'cargos';
+$obj->tipo = 'retiros';
+$obj->detalle = 'transferencia a tercero';
+$obj->referencia = '8762778';
+$obj->institucion = 'amazon';
+$obj->tasa = '';
+$obj->cantidad = '';
+$obj->unitario = '-25.00';
+$obj->subtotal = '-25.00';
+$obj->saldo = '491.00';
+$obj->notas = '';
+$arregloobjetos[22] = $obj;
+
+$obj = new stdClass;
+$obj->fecha = '28-feb';
+$obj->movimiento = 'abono';
+$obj->tipo = 'retorno';
+$obj->detalle = 'de financiamientos';
+$obj->referencia = '63546345';
+$obj->institucion = 'preferente';
+$obj->tasa = '';
+$obj->cantidad = '2';
+$obj->unitario = '-175.00';
+$obj->subtotal = '350.00';
+$obj->saldo = '841.00';
+$obj->notas = '';
+$arregloobjetos[23] = $obj;
+
+
+
+$arregloCatalogo = Array();
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 0;
+$catalogoTipo->name = 'transferencia';
+$arregloCatalogo[0] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 1;
+$catalogoTipo->name = 'deposito';
+$arregloCatalogo[1] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 2;
+$catalogoTipo->name = 'inversion';
+$arregloCatalogo[2] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 3;
+$catalogoTipo->name = 'retorno';
+$arregloCatalogo[3] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 4;
+$catalogoTipo->name = 'retiros';
+$arregloCatalogo[4] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 5;
+$catalogoTipo->name = 'impuestos';
+$arregloCatalogo[5] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 6;
+$catalogoTipo->name = 'reembolso';
+$arregloCatalogo[6] = $catalogoTipo;
+
+$catalogoTipo = new stdClass;
+$catalogoTipo->id = 7;
+$catalogoTipo->name = 'Retiro';
+$arregloCatalogo[7] = $catalogoTipo;
+
+
+?>
