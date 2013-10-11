9c9e7e2bc2ee1ccffb95b6c546718346c3869c61
lutek <lutek.valhalla@gmail.com>
2013-10-11 12:04:23 -0500

    Formulario abono y agregar una etiqueta(evitar merge)

--- freakfund/components/com_jumi/files/abono_socio/abonoSocio.php
+++ freakfund/components/com_jumi/files/abono_socio/abonoSocio.php
@@ -10,17 +10,22 @@ if ($usuario->guest == 1) {
 defined('_JEXEC') OR defined('_VALID_MOS') OR die( "Direct Access Is Not Allowed" );
 jimport('trama.class');
 $token = JTrama::token();
-$callback = JURI::base().'index.php?option=com_jumi&view=application&fileid=29&proyid=';;
-$errorCallback =JURI::base().'index.php?option=com_jumi&view=application&fileid=29&proyid=';;
+$callback = JURI::base().'index.php?option=com_jumi&view=application&fileid=29&proyid=';
+$errorCallback =JURI::base().'index.php?option=com_jumi&view=application&fileid=29&proyid=';
+$usuario = JFactory::getUser();
 ?>
 
 <form>
-	<div>
-		<label for="amount"><?php JText::_('FREAKFUND_JUMI_ABONOSOCIO_MONTO'); ?></label><input type="text" name="amount" id="amount" />
-	</div>
 	<div id="hiddens">
 		<input type="hidden" name="callback" id="callback" value="<?php echo $callback; ?>" />
 		<input type="hidden" name="errorCallback" id="errorCallback" value="<?php echo $errorCallback; ?>" />
 		<input type="hidden" name="token" id="token" value="<?php echo $token; ?>" />
+		<input type="hidden" name="userId" id="userId" value="<?php echo $usuario->id; ?>" />
+	</div>
+	<div>
+		<label for="amount"><?php echo JText::_('MONTO'); ?></label><input type="text" name="amount" id="amount" />
+	</div>
+	<div>
+		<input type="button" id="abonar" value="<?php echo JText::_('FREAKFUND_JUMI_ABONOSOCIO_ABONAR'); ?>"/>
 	</div>
 </form>
