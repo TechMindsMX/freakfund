bc9a3c1b40c116023caeb51719f851454704c59c
Ricardo <ricardolyon@gmail.com>
2013-10-28 16:08:28 -0600

    evita los notice cuando no hay pro..

--- freakfund/modules/mod_carrete/tmpl/default.php
+++ freakfund/modules/mod_carrete/tmpl/default.php
@@ -65,31 +65,31 @@ $url = 'index.php?option=com_jumi&view=appliction&fileid=11&proyid=';
 	 		foreach ($datos as $key => $value) {
 				echo '<li>
 	    				<div class="contenedor productos">
-	    					<div class="avatar" style="background:url(\''.AVATAR.'/'.$value->projectAvatar->name.'\'); background-size: 100%;">
+	    					<div class="avatar" style="background:url(\''.AVATAR.'/'.@$value->projectAvatar->name.'\'); background-size: 100%;">
 	    					</div>
 	    					<div class="info-proyecto" >
 								<div class="titulo">
-								<a href="'.$url.$value->id.'">
-								<h2>'.$value->name.'</h2>
+								<a href="'.$url.@$value->id.'">
+								<h2>'.@$value->name.'</h2>
 								</a>
 								</div>
 								<div class="cat">
-								<span>'.$value->categoryName.'</span> - 
-								<span>'.$value->subcategoryName.'</span>
+								<span>'.@$value->categoryName.'</span> - 
+								<span>'.@$value->subcategoryName.'</span>
 								</div>
 								<div class="datos">
 								<div class="two-cols first">
-								<div class="big">'.$value->ROI.'%</div>
+								<div class="big">'.@$value->ROI.'%</div>
 								<div class="small">'.JText::_('LABEL_ROI').'</div>
 								</div>
 								<div class="two-cols second">
-								<div class="big">'.$value->ROF.'%</div>
+								<div class="big">'.@$value->ROF.'%</div>
 								<div class="small">'.JText::_('LABEL_ROF').'</div>
 								</div>
 								<div class="clearfix"></div>
 								</div>
 								<div class="boton-wrap">
-									<a class="button btn-invertir" href="'.$url.$value->id.'">
+									<a class="button btn-invertir" href="'.$url.@$value->id.'">
 										'.JText::_('INVERTIR_PROYECTO').'</a>
 	    						</div>
 	    					</div>
@@ -103,26 +103,26 @@ $url = 'index.php?option=com_jumi&view=appliction&fileid=11&proyid=';
 	 			$value->porcentajeRecaudado = 30;
 				echo '<li>
 	    				<div class="contenedor proyectos">
-	    					<div class="avatar" style="background:url(\''.AVATAR.'/'.$value->projectAvatar->name.'\'); background-size: 100%;">
+	    					<div class="avatar" style="background:url(\''.AVATAR.'/'.@$value->projectAvatar->name.'\'); background-size: 100%;">
 		    					<span class="mask"></span>
 	    					</div>
 	    					<div class="info-proyecto" >
 								<div class="titulo">
-								<a href="'.$url.$value->id.'">
-								<h2>'.$value->name.'</h2>
+								<a href="'.$url.@$value->id.'">
+								<h2>'.@$value->name.'</h2>
 								</a>
 								</div>
 								<div class="cat">
-								<span>'.$value->categoryName.'</span> - 
-								<span>'.$value->subcategoryName.'</span>
+								<span>'.@$value->categoryName.'</span> - 
+								<span>'.@$value->subcategoryName.'</span>
 								</div>
-								<div class="barra" style="width: '.$value->porcentajeRecaudado.'% "></div>
+								<div class="barra" style="width: '.@$value->porcentajeRecaudado.'% "></div>
 								<div class="cat texto-normal recaudado">'.JText::_('LABEL_RECAUDADO').
-								'<span class="number">'.$value->balance.'</span>
+								'<span class="number">'.@$value->balance.'</span>
 								</div>
 								<div class="cat texto-normal dias-restantes">'.JText::sprintf('LAPSED_DAYS', $value->dateDiff->days).'</div>
 								<div class="boton-wrap">
-									<a class="button btn-invertir" href="'.$url.$value->id.'">
+									<a class="button btn-invertir" href="'.$url.@$value->id.'">
 										'.JText::_('INVERTIR_PROYECTO').'</a>
 	    						</div>
 	    					</div>
@@ -135,18 +135,18 @@ $url = 'index.php?option=com_jumi&view=appliction&fileid=11&proyid=';
 	 		foreach ($datos as $key => $value) {
 				echo '<li>
 	    				<div class="contenedor">
-	    					<div style="background:url(\''.AVATAR.'/'.$value->projectAvatar->name.'\'); background-size: 100%;">
-	    						<img src="'.AVATAR.'/'.$value->projectAvatar->name.'" class="imagenes" />
+	    					<div style="background:url(\''.AVATAR.'/'.@$value->projectAvatar->name.'\'); background-size: 100%;">
+	    						<img src="'.AVATAR.'/'.@$value->projectAvatar->name.'" class="imagenes" />
 	    					</div>
 	    					<div class="info-proyecto" >
-								<div><h3>'.$value->name.'</h3></div>
-								<span>'.JText::_('CATEGORIA').' '.$value->categoryName.'</span> - 
-								<span>'.$value->subcategoryName.'</span><br />
-								<span>'.$value->status.'</span><br />
-								<span>'.JText::_('LABEL_ROI').' '.$value->ROI.'%</span><br />
-								<span>'.JText::_('LABEL_ROF').' '.$value->ROF.'%</span><br />
+								<div><h3>'.@$value->name.'</h3></div>
+								<span>'.JText::_('CATEGORIA').' '.@$value->categoryName.'</span> - 
+								<span>'.@$value->subcategoryName.'</span><br />
+								<span>'.@$value->status.'</span><br />
+								<span>'.JText::_('LABEL_ROI').' '.@$value->ROI.'%</span><br />
+								<span>'.JText::_('LABEL_ROF').' '.@$value->ROF.'%</span><br />
 								<div class="boton-wrap">
-									<a class="button btn-invertir" href="'.$url.$value->id.'">
+									<a class="button btn-invertir" href="'.$url.@$value->id.'">
 										'.JText::_('INVERTIR_PROYECTO').'</a>
 	    						</div>
 	    					</div>
