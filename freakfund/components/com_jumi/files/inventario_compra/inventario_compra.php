7c0b86bd8b4b58bd15b41136d6f90fa152ab350a
Ricardo <ricardolyon@gmail.com>
2013-10-28 15:45:55 -0600

    estilos y etiquetas compra y resultados de compra

--- freakfund/components/com_jumi/files/inventario_compra/inventario_compra.php
+++ freakfund/components/com_jumi/files/inventario_compra/inventario_compra.php
@@ -37,6 +37,14 @@ echo '<script src="'.JURI::base().'libraries/trama/js/jquery.number.min.js"> </s
 <script>
 jQuery(document).ready(function(){
 	jQuery("span.number").number(true,2,',','.');
+	
+		var $scrollingDiv = $("#scrollingDiv");
+ 
+		jQuery(window).scroll(function(){			
+			$scrollingDiv
+				.stop()
+				.animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );			
+		});
 });
 </script>
 <?php
@@ -102,34 +110,36 @@ jQuery(document).ready(function(){
 <h2><?php echo $pro->name; ?></h2>
 <div>
 	<form id="form_compra" action="<?php echo $action; ?>" method="POST">
-		<input type="hidden" name="userId" id="userId" value="<?php echo $idMiddleware->idMiddleware; ?>" />
-		<input type="hidden" name="projectId" id="projectId" value="<?php echo $pro->id; ?>" />
-	
 		<?php
+		$html = '<input type="hidden" name="userId" id="userId" value="'. $idMiddleware->idMiddleware .'" />
+				<input type="hidden" name="projectId" id="projectId" value="'. $pro->id .'" />';
+	
 		foreach ($pro->projectUnitSales as $key => $value){
 
-			$html = '<div class="wrapper">
-					<div>'.JText::_('SECCION').':'. $value ->section .'</div>
+			$html .= '<div class="wrapper">
+					<div>'.JText::_('SECCION').': '. $value ->section .'</div>
 					<div>'.JText::_('PRECIO_UNIDAD').': <span class="number valor_unidad">'. $value ->unitSale.'</span></div>
 					<div>'.JText::_('INVENTARIOPP').': <span>'. $value ->unit .'</span></div>
 					<input type="hidden" value="'.$value ->id.'"/>
 					<label>'.JText::_('CANTIDAD_COMPRAR').':</label>
 					<input class="input_compra" type="text" id="'.$value->id.'" name="" />
 					<div>'.JText::_('TOTAL_SECCION').': '.'<span class="number" id="resultados"></span></div>
+					<hr />
 					</div>';
-			echo $html;
 		}
 
-		echo '<h3>'. $usuario->name .'</h3>';
-		echo '<div>'.JText::_('SALDO_FF').': '. $saldo .'</div>';
-		echo '<div>'.JText::_('TOTAL_PAGAR').': <span class="number" id="resultadoglobal"></span></div>';
+		$html .= '<h3>'. $usuario->name .'</h3>
+				<div>'.JText::_('SALDO_FF').': <span class="number">'. $saldo .'</span></div>
+				<div>'.JText::_('TOTAL_PAGAR').': <span class="number" id="resultadoglobal"></span></div>
+				<input type="hidden" name="callback" value="'. $callback .'" />
+				<input type="hidden" name="token" value="'. $token .'" />
+				<div style="margin: 10px;">
+					<input type="submit" id="guardar" class="button" value="'.JText::_('INVERTIR_PROYECTO').'" />
+					<input type="button" class="button" value="'.JText::_('CANCELAR').'" onclick="history.go(-1);" />
+				</div>';
+
+			echo $html;
 		?>
-		<input type="hidden" name="callback" value="<?php echo $callback; ?>" />
-		<input type="hidden" name="token" value="<?php echo $token; ?>" />
-		<div style="margin: 10px;">
-		<input type="submit" id="guardar" class="button" value="Invertir" />
-		<input type="button" class="button" value="Cancelar" onclick="history.go(-1);" />
-		</div>
 	</form>
 	
 </div>
@@ -140,11 +150,11 @@ jQuery(document).ready(function(){
 	
 	$html = '<h2>'.$usuario->name.'</h2>
 			<div>'.JText::_('SALDO_FF').': <span class="number">'. $saldo .'</span></div>
-			<h3>'.$pro->name.'</h3>
 			<p>'.JText::_('COMPRA_SUCCESS').'</p>
 			<h3>'.JText::_('COMPRA_SUCCESS_DETAILS').'</h3>
 			<div class="detalles_tx">
-				<span class="label">'.JText::_('COMPRA_TX_ID').' : </span>'.$response.'
+				<h3>'.$pro->name.'</h3>
+				<span>'.JText::_('COMPRA_TX_ID').' : </span>'.$response.'
 				<table class="table table-stripped">
 					<tr>
 						<th>'.JText::_('SECCION').'</th>
@@ -153,16 +163,16 @@ jQuery(document).ready(function(){
 						<th>'.JText::_('COMPRA_SUBTOTAL').'</th>
 					</tr>';
 	foreach ($pro->projectUnitSales as $key => $value) {
-	$cant = 3;
-	$subtotal = $value->unitSale*$cant;
-	$totalCompra = $totalCompra + $subtotal;
-	
-	$html .=	'<tr>
-					<td>'.$value->section.'</td>
-					<td><span class="number">'.$value->unitSale.'</span></td>
-					<td>'.$cant.'</td>
-					<td><span class="number">'.$subtotal.'</span></td>
-				</tr>';
+		$cant = 3;
+		$subtotal = $value->unitSale*$cant;
+		$totalCompra = $totalCompra + $subtotal;
+		
+		$html .=	'<tr>
+						<td>'.$value->section.'</td>
+						<td><span class="number">'.$value->unitSale.'</span></td>
+						<td>'.$cant.'</td>
+						<td><span class="number">'.$subtotal.'</span></td>
+					</tr>';
 	}
 	
 	$html .=	'<tr>
@@ -170,6 +180,7 @@ jQuery(document).ready(function(){
 					<td><b><span class="number">'.$totalCompra.'</span></b></td>
 				</tr>
 				</table>
+				<a class="button" href="index.php?option=com_jumi&view=application&fileid=24&Itemid=218">'.JText::_('ESCRIT').'</a>
 					<pre>SIMULADO FALTA CREAR SERVICIO PARA REGRESAR DATOS DE UNA TX</pre>
 				</div>
 				';
