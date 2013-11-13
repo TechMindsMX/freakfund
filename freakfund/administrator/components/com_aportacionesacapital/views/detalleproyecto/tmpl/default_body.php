b7771283020a5d6cbd7d4ed693d4d145b0f7a247
lutek <lutek.valhalla@gmail.com>
2013-11-13 15:51:53 -0600

    vista detalle del proyecto com_aporteacapital

--- freakfund/administrator/components/com_aportacionesacapital/views/detalleproyecto/tmpl/default_body.php
+++ freakfund/administrator/components/com_aportacionesacapital/views/detalleproyecto/tmpl/default_body.php
@@ -3,27 +3,26 @@
 defined('_JEXEC') or die('Restricted Access');
 jimport('trama.usuario_class');
 $datos = $this->items;
-//var_dump($datos);exit;
-
+//var_dump($datos->providers);exit;
 ?>
 	<tr>
 		<td align="justify">
 			<div>
-				<div class="textos"><span class="labels">Fecha de creación:</span></div>
+				<div class="textos"><span class="labels"><?php echo JText::_('COM_APORTACIONESCAPITAL_DETALLEPROYECTO_CREACION'); ?>:</span></div>
 				<div class="textos derecha"><?php echo date('d-m-Y', ($datos->timeCreated/1000)); ?></div>
 			</div>
 			<div>
-				<div class="textos"><span class="labels">Punto de equilibrio:</span></div>
+				<div class="textos"><span class="labels"><?php echo JText::_('COM_APORTACIONESCAPITAL_LISTADOPROYECTOS_HEADING_BREAKEVEN'); ?>:</span></div>
 				<div class="textos derecha">$<span class="number"><?php echo $datos->breakeven; ?></span></div>
 			</div>
 			
 			<div>
-				<div class="textos"><span class="labels">Recaudado:</span></div>
+				<div class="textos"><span class="labels"><?php echo JText::_('COM_APORTACIONESCAPITAL_DETALLEPROYECTO_RECAUDADO'); ?>:</span></div>
 				<div class="textos derecha">$<span class="number"><?php echo $datos->balance; ?></span></div>
 			</div>
 			
 			<div>
-				<div class="textos"><span class="labels">Por recaudar:</span></div>
+				<div class="textos"><span class="labels"><?php echo JText::_('COM_APORTACIONESCAPITAL_DETALLEPROYECTO_FALTANTE'); ?>:</span></div>
 				<div class="textos derecha">$<span class="number"><?php echo $datos->breakeven-$datos->balance; ?></span></div>
 			</div>
 			
@@ -35,7 +34,9 @@ $datos = $this->items;
 				echo "<ul>";
 				foreach ($datos->providers as $key => $value) {
 					if( $value->producerName != 'Super User' && ($value->providerId != $datos->userId) ){
-						echo '<li>'.$value->producerName.'</li>';
+						echo '<li>';
+						echo '<a href="index.php?option=com_aportacionesacapital&task=aporteproveedor&id=1&providerId='.$value->providerId.'">'.$value->producerName.'</a>';
+						echo '</li>';
 					}
 				}
 				echo "</ul>";
