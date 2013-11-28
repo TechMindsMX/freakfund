afc940c840db8a5efd10eb3a61e185a94a41a260
lutek <lutek.valhalla@gmail.com>
2013-11-28 14:21:50 -0600

    alta de traspasos pendiente validaciones

--- freakfund/components/com_jumi/files/alta_traspasos/alta_traspasos.php
+++ freakfund/components/com_jumi/files/alta_traspasos/alta_traspasos.php
@@ -25,6 +25,24 @@ $beneficiarios 	= UserData::getBeneficiarios($userId->idMiddleware);
 	jQuery(document).ready(function(){
 		jQuery('#clabe').change(function(){
 			var clabe = this.value;
+			var salir = false;
+			
+			jQuery('.fila').each(function(){
+			    if( parseInt(jQuery(this).find('.numCuenta').html()) == parseInt(clabe) ){
+			    	alert('La cuenta ya esta dada de alta');
+			    	salir = true;
+			    }
+			    
+			});
+			
+			if( salir ){
+				jQuery('#socio').val('');
+				jQuery('#email').val('');
+				jQuery('#clabe').val('');
+				jQuery('#guardar').attr('disabled', 'disabled');
+				return false;
+			}
+			
 			var request = $.ajax({
 				url: "libraries/trama/js/ajax.php",
 				data: {
@@ -38,6 +56,7 @@ $beneficiarios 	= UserData::getBeneficiarios($userId->idMiddleware);
 			request.done(function(result){
 				if(result != ''){
 					var obj = eval('(' + result + ')');
+					
 					if(!obj.error){
 						jQuery('#socio').val(obj.name);
 						jQuery('#email').val(obj.email);
@@ -77,14 +96,15 @@ $beneficiarios 	= UserData::getBeneficiarios($userId->idMiddleware);
 		});
 		
 		jQuery('#guardar').click(function(){
-			jQuery('#showSocio').html(jQuery('#socio').val());
-			jQuery('#showmaxmount').html('$<span class="number">' + jQuery('#maxMount').val() + '</span>' );
-			jQuery('#showemail').html(jQuery('#email').val());
-			jQuery('#showClabe').html(jQuery('#clabe').val());
+			var nombre 	= jQuery('#socio').val();
+			var monto	= jQuery('#maxMount').val();
+			var email 	= jQuery('#email').val();
+			var numCta 	= jQuery('#clabe').val();
+			var action 	= 'guardar';
+			var message = '<?php echo JText::_('ALTA_TRASPASOS_MSG_GUARDAR'); ?>';
+			var div		= jQuery(this).parent().parent();
 			
-			jQuery("span.number").number( true, 2 );
-			
-			jQuery('#divConfirmacion').show();
+			pintadivConfirmacion(nombre, monto, email, numCta, action, message, div);
 		});
 		
 		jQuery('#Cancelar').click(function(){
@@ -97,122 +117,189 @@ $beneficiarios 	= UserData::getBeneficiarios($userId->idMiddleware);
 			jQuery('#maxMount').val('');
 			jQuery('#email').val('');
 			jQuery('#clabe').val('');
-			
+			jQuery('#autocompletado').find('#guardar').attr('disabled', 'disabled');
 			jQuery('#divConfirmacion').hide();
 			jQuery('#divFormulario').show();
 		});
 	});
+	
+	
+	function confirmaciondelete(campo){
+		var montoMaximo 		= jQuery(campo).parent().parent().find('.editable').find('input').val();
+		var numCuenta 			= parseInt(jQuery(campo).parent().parent().find('.numCuenta').html());
+		var nombreBeneficiario	= jQuery(campo).parent().parent().find('.nomBeneficiario').html();
+		var mailBeneficiario	= jQuery(campo).parent().parent().find('.mailBeneficiario').html();
+		var action				= 'borrar';
+		var message				= '<?php echo JText::_('ALTA_TRASPASOS_MSG_BORRAR'); ?>';
+		var div 				= jQuery(campo).parent().parent()
+		
+		pintadivConfirmacion(nombreBeneficiario, montoMaximo, mailBeneficiario, numCuenta, action, message, div);
+	}
+	
+	function confirmacionUpdate(campo){
+		
+		var montoMaximo 		= jQuery(campo).parent().parent().find('.editable').find('input').val();
+		var numCuenta 			= parseInt(jQuery(campo).parent().parent().find('.numCuenta').html());
+		var nombreBeneficiario	= jQuery(campo).parent().parent().find('.nomBeneficiario').html();
+		var mailBeneficiario	= jQuery(campo).parent().parent().find('.mailBeneficiario').html();
+		var action				= 'update';
+		var message				= '<?php echo JText::_('ALTA_TRASPASOS_MSG_UPDATE'); ?>';
+		var div 				= jQuery(campo).parent().parent()
+
+		pintadivConfirmacion(nombreBeneficiario, montoMaximo, mailBeneficiario, numCuenta, action, message, div);
+	}
+	
+	function pintadivConfirmacion(nombre, monto, email, numCta, action, message, div){
+		console.log(div);
+		
+		jQuery('#showSocio').html(nombre);
+		jQuery('#showmaxmount').html('$<span class="number">' + monto + '</span>' );
+		jQuery('#showemail').html(email);
+		jQuery('#showClabe').html(numCta);
+		
+		jQuery("span.number").number( true, 2 );
+		
+		switch(action){
+			case 'guardar':
+				$('#divConfirmacion').find('h3').html(message);
+				$('#divConfirmacion').find('.safe').attr('onclick','safeUpdate(this)');
+				break;
+			case 'borrar':
+				$('#divConfirmacion').find('h3').html(message);
+				$('#divConfirmacion').find('.safe').attr('onclick','deleteCta(this)');
+				break
+			case 'update':
+				$('#divConfirmacion').find('h3').html(message);
+				$('#divConfirmacion').find('.safe').attr('onclick','safeUpdate(this)');
+				break;
+		}
+		
+		jQuery('#divConfirmacion').show();
+	}
 	
 	function deleteCta(campo){
 		var action 			= campo;
 		var userId 			= jQuery('#userId').val();
 		var destinationId	= jQuery(campo).parent().parent().find('#destinationIdEdicion').val();
 		
-		
-		var request = $.ajax({
-			url: "<?php echo MIDDLE.PUERTO; ?>/trama-middleware/rest/tx/deleteLimitAmountToTransfer",
-			data: {
-				"userId"		: userId,
-				"destinationId"	: destinationId,
-				"token"			: "<?php echo $token; ?>"
-			},
-			type: 'post'
-		});
-		
-		request.done(function(result){
-			jQuery(campo).parent().parent().remove();
-		});
-		
-		request.fail(function (jqXHR, textStatus) {
-			alert("Ocurrio un problema al eliminar");
-		});
+		if(confirm('Esta seguro de Borrar el numero de cuenta seleccionado')){
+			var request = $.ajax({
+				url: "<?php echo MIDDLE.PUERTO; ?>/trama-middleware/rest/tx/deleteLimitAmountToTransfer",
+				data: {
+					"userId"		: userId,
+					"destinationId"	: destinationId,
+					"token"			: "<?php echo $token; ?>"
+				},
+				type: 'post'
+			});
+			
+			request.done(function(result){
+				jQuery(campo).parent().parent().remove();
+			});
+			
+			request.fail(function (jqXHR, textStatus) {
+				alert("Ocurrio un problema al eliminar");
+			});
+		} else {
+			return false;
+		}
 	}
 	
 	function safeUpdate(campo){
 		var action 			= campo;
 		var userId 			= jQuery('#userId').val();
-		if(action.value == "Confirmar"){
-			var maxAmount 		= jQuery('#maxMount').val();
-			var destinationId	= jQuery('#destinationId').val();
-		}else{
-			var maxAmount 		= jQuery(campo).parent().parent().find('input[type="text"]').val();
-			var destinationId	= jQuery(campo).parent().parent().find('#destinationIdEdicion').val();
-		}
 		
-		var request = $.ajax({
-			url: "<?php echo MIDDLE.PUERTO; ?>/trama-middleware/rest/tx/maxAmountToTransfer",
-			data: {
-				"userId"		: userId,
-				"amount"		: maxAmount,
-				"destinationId"	: destinationId,
-				"token"			: "<?php echo $token; ?>"
-			},
-			type: 'post'
-		});
-		
-		request.done(function(result){
-			if(action.value == "Confirmar"){
-				var html = '';
-				
-				html += '<div class="fila">';
-				html += '	<input type="hidden" id="destinationIdEdicion" value="' +destinationId+ '" />';
-				html += '	<div class="editable" onclick="editar(this)">';
-				html += '		<input type="hidden" value="'+maxAmount+'" />';
-				html += '		<span>$<span class="number">'+maxAmount+'</span></span>';
-				html += '	</div>';
-				html += '	<div>';
-				html += '	<input type="hidden" value="'+jQuery('#clabe').val()+'" />';
-				html += '	<span>'+jQuery('#clabe').val()+'</span>';
-				html += '	</div>';
-				html += '	<div>'+jQuery('#socio').val()+'</div>';
-				html += '	<div>'+jQuery('#email').val()+'</div>';
-				html += '	<div style="width: 170px;">';
-				html += '		<input type="button" class="button safe" value="Actualizar" onclick="safeUpdate(this)" disabled="disabled" />';
-				html += '		<input type="button" class="button" value="Borrar" onclick="deleteCta(this)" />';
-				html += '	</div>';
-				html += '</div>';
-				
-				jQuery(html).insertBefore('#autocompletado');
-				
-				jQuery("span.number").number( true, 2 );
-				
-				jQuery('#showSocio').html();
-				jQuery('#showmaxmount').html();
-				jQuery('#showemail').html();
-				jQuery('#showClabe').html();
-				
-				jQuery('#socio').val('');
-				jQuery('#maxMount').val('');
-				jQuery('#email').val('');
-				jQuery('#clabe').val('');
-				jQuery('#destinationId').val('');
-				
-				jQuery('#divConfirmacion').hide();
-				jQuery('#divFormulario').show();
-			}else if(action.value == "Actualizar") {
-				jQuery(action).parent().parent().find('.safe').attr('disabled', 'disabled');
-				
-				jQuery(action).parent().parent().find('span').text('');
-				jQuery(action).parent().parent().find('span').html('$<span class="number">' + jQuery(action).parent().parent().find('input[type="text"]').val() + '</span>');
-				
-				jQuery(action).parent().parent().find('input[type="text"]').prop('type', 'hidden');
-				
-				jQuery("span.number").number( true, 2 );
-				
-				jQuery(action).parent().parent().find('span').show();
-			}
-		});
-		
-		request.fail(function (jqXHR, textStatus) {
-			alert("Ocurrio un problema al crear/actualizar los datos");
-		});
+		// if(action.value == "Confirmar"){
+			// var maxAmount 		= jQuery('#maxMount').val();
+			// var destinationId	= jQuery('#destinationId').val();
+		// }else{
+			// var maxAmount 		= jQuery(campo).parent().parent().find('input[type="text"]').val();
+			// var destinationId	= jQuery(campo).parent().parent().find('#destinationIdEdicion').val();
+		// }
+// 		
+		// var request = $.ajax({
+			// url: "<?php echo MIDDLE.PUERTO; ?>/trama-middleware/rest/tx/maxAmountToTransfer",
+			// data: {
+				// "userId"		: userId,
+				// "amount"		: maxAmount,
+				// "destinationId"	: destinationId,
+				// "token"			: "<?php echo $token; ?>"
+			// },
+			// type: 'post'
+		// });
+// 		
+		// request.done(function(result){
+			// var obj = eval('(' + result + ')');
+			// if(action.value == "Confirmar"){
+				// var html = '';
+// 				
+				// html += '<div class="fila" id="'+obj.response+'">';
+				// html += '	<input type="hidden" id="destinationIdEdicion" value="' +destinationId+ '" />';
+				// html += '	<div class="editable" onclick="editar(this)">';
+				// html += '		<input type="hidden" value="'+maxAmount+'" />';
+				// html += '		<span>$<span class="number">'+maxAmount+'</span></span>';
+				// html += '	</div>';
+				// html += '	<div>';
+				// html += '	<input type="hidden" value="'+jQuery('#clabe').val()+'" />';
+				// html += '	<span>'+jQuery('#clabe').val()+'</span>';
+				// html += '	</div>';
+				// html += '	<div>'+jQuery('#socio').val()+'</div>';
+				// html += '	<div>'+jQuery('#email').val()+'</div>';
+				// html += '	<div style="width: 170px;">';
+				// html += '		<input type="button" class="button safe" value="Actualizar" onclick="confirmacionUpdate(this)" disabled="disabled" />';
+				// html += '		<input type="button" class="button" value="Borrar" onclick="confirmaciondelete(this)" />';
+				// html += '	</div>';
+				// html += '</div>';
+// 				
+				// jQuery(html).insertBefore('#autocompletado');
+// 				
+				// jQuery("span.number").number( true, 2 );
+// 				
+				// jQuery('#showSocio').html();
+				// jQuery('#showmaxmount').html();
+				// jQuery('#showemail').html();
+				// jQuery('#showClabe').html();
+// 				
+				// jQuery('#socio').val('');
+				// jQuery('#maxMount').val('');
+				// jQuery('#email').val('');
+				// jQuery('#clabe').val('');
+				// jQuery('#destinationId').val('');
+// 				
+				// jQuery('#divConfirmacion').hide();
+				// jQuery('#divFormulario').show();
+			// }else if(action.value == "Actualizar") {
+				// jQuery(action).parent().parent().find('.safe').attr('disabled', 'disabled');
+// 				
+				// jQuery(action).parent().parent().find('span').text('');
+				// jQuery(action).parent().parent().find('span').html('$<span class="number">' + jQuery(action).parent().parent().find('input[type="text"]').val() + '</span>');
+// 				
+				// jQuery(action).parent().parent().find('input[type="text"]').prop('type', 'hidden');
+// 				
+				// jQuery("span.number").number( true, 2 );
+// 				
+				// jQuery(action).parent().parent().find('span').show();
+			// }
+		// });
+// 		
+		// request.fail(function (jqXHR, textStatus) {
+			// alert("Ocurrio un problema al crear/actualizar los datos");
+		// });
 	}
 	
 	function editar(campo){
+		jQuery('#formAltaTraspaso').find('.fila').each(function(){
+			if( jQuery.isNumeric(this.id) ){
+				jQuery(this).find('.safe').attr('disabled', 'disabled');
+				jQuery(this).find('span').show();
+				jQuery(this).find('input[type="text"]').hide();
+			}
+		});
+
 		jQuery(campo).parent().find('.safe').prop('disabled', false);
-		
 		jQuery(campo).find('span').hide();
-		jQuery(campo).find('input').prop('type', 'text')
+		jQuery(campo).find('input').prop('type', 'text');
+		jQuery(campo).find('input[type="text"]').show();
 	}
 </script>
 
@@ -233,21 +320,21 @@ $beneficiarios 	= UserData::getBeneficiarios($userId->idMiddleware);
 		//Se crea el listado de los usuario dados de alta
 		foreach ($beneficiarios as $key => $value) {
 		?>
-			<div class="fila" id="<?php echo $key; ?>">
+			<div class="fila" id="<?php echo $value->id; ?>">
 				<input type="hidden" id="destinationIdEdicion" value="<?php echo $value->destinationId; ?>" />
 
 				<div class="editable" onclick="editar(this)" >
 					<input type="hidden" value="<?php echo $value->amount; ?>" />
 					<span>$<span class="number"><?php echo $value->amount; ?></span></span>
 				</div>
-				<div>
+				<div class="numCuenta">
 					<?php echo $value->account; ?>
 				</div>
-				<div><?php echo $value->name; ?></div>
-				<div><?php echo $value->email; ?></div>
+				<div class="nomBeneficiario"><?php echo $value->name; ?></div>
+				<div class="mailBeneficiario"><?php echo $value->email; ?></div>
 				<div style="width: 170px;">
-					<input type="button" class="button safe" value="Actualizar" onclick="safeUpdate(this)" disabled="disabled" />
-					<input type="button" class="button" value="Borrar" onclick="deleteCta(this)" />
+					<input type="button" class="button safe" value="Actualizar" onclick="confirmacionUpdate(this)" disabled="disabled" />
+					<input type="button" class="button" value="Borrar" onclick="confirmaciondelete(this)" />
 				</div>
 			</div>
 		<?php 
@@ -256,7 +343,7 @@ $beneficiarios 	= UserData::getBeneficiarios($userId->idMiddleware);
 		
 		<!--Campos para dar de alta un numero de cuenta-->
 		<div class="fila" id="autocompletado">
-			<div><input type="text" name="maxMount" id="maxMount" class="altavalidation"/></div>
+			<div><input type="text" name="maxMount" id="maxMount" class="altavalidation" placeholder="Monto maximo"/></div>
 			<div><input type="text" name="clabe" id="clabe"  class="altavalidation" maxlength="10" /></div>
 			<div><input type="text" name="socio" id="socio" readonly="readonly" /></div>
 			<div><input type="text" name="email" id="email" readonly="readonly" /></div>
