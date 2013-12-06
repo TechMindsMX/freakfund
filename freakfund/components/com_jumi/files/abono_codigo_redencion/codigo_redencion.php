c40492e3d2941afe76ba5a945e778f3158dd7d78
lutek <lutek.valhalla@gmail.com>
2013-12-06 10:47:15 -0600

    agregar select con el listado de los proyectos con estatus 5,6 y 7

--- freakfund/components/com_jumi/files/abono_codigo_redencion/codigo_redencion.php
+++ freakfund/components/com_jumi/files/abono_codigo_redencion/codigo_redencion.php
@@ -75,33 +75,44 @@ $action = '#';
 <div>
 	<form id="form_codigo" action="<?php echo $action; ?>" method="POST">
 	
-		<?php echo JText::_('CODIGO_PROMO');  ?> <input type="text" class="validate[required,custom[number]]" id="codigo"name="codigo">
-		
-		 <script type="text/javascript"
+	<div>
+		<label for="proys"><?php echo JText::_('CODIGO_PROYECTOS'); ?></label>
+		<select name="proys">
+		<?php
+			$proyectos = JTrama::getProyByStatus('5,6,7');
+			foreach($proyectos as $key => $value){
+				echo '<option value="'.$value->id.'">'.$value->name.'</option>';
+			}
+		?>
+		</select>
+	</div>
+	
+	<div>
+		<label for="codigo" ><?php echo JText::_('CODIGO_PROMO');  ?></label> 
+		<input type="text" class="validate[required,custom[number]]" id="codigo" name="codigo">
+	</div>
+	
+	<script type="text/javascript"
        src="http://www.google.com/recaptcha/api/challenge?k=6LeDLOgSAAAAAIfown4LiNrDQgkwPVZ9FOG6moog">
     </script>
+    
     <noscript>
-       <iframe src="http://www.google.com/recaptcha/api/noscript?k=6LeDLOgSAAAAAIfown4LiNrDQgkwPVZ9FOG6moog"
-           height="300" width="500" frameborder="0"></iframe><br>
-       <textarea name="recaptcha_challenge_field" rows="3" cols="40">
-       </textarea>
-       <input type="hidden" name="recaptcha_response_field"
-           value="manual_challenge">
+       <iframe src="http://www.google.com/recaptcha/api/noscript?k=6LeDLOgSAAAAAIfown4LiNrDQgkwPVZ9FOG6moog" height="300" width="500" frameborder="0"></iframe>
+       <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+       <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
     </noscript>
 		
-		<div style="margin: 10px;">
-			<input type="button" class="button" value="<?php echo JText::_('CANCELAR');  ?>" onClick="if(confirm('<?php echo JText::_('CONFIRMAR_CANCELAR');  ?>'))
-		javascript:window.history.back();">
-			<input type="button" class="button" id="enviar" value="Redimir" />
-		</div>
-		
-		<div id="ajax_done" style="display: none">
+	<div style="margin: 10px;">
+		<input type="button" class="button" value="<?php echo JText::_('CANCELAR');  ?>" onClick="if(confirm('<?php echo JText::_('CONFIRMAR_CANCELAR');  ?>'))
+	javascript:window.history.back();">
+		<input type="button" class="button" id="enviar" value="Redimir" />
+	</div>
+	
+	<div id="ajax_done" style="display: none">
 		Producto invertido:	<span id="producto"></span><br>
 		Monto invertido:	<span id="monto"></span><br>
 		Tasa de retorno al momento:	<span id="tasa"></span>
-			
-		
-		</div>
+	</div>
 		
 	</form>
 	
