781edd73a1332ca77f3c5dc1335c2ebb0fb152bb
lutek <lutek.valhalla@gmail.com>
2013-12-11 14:36:20 -0600

    pantalla confirmacion inventario compra

--- freakfund/components/com_jumi/files/inventario_compra/inventario_compra.php
+++ freakfund/components/com_jumi/files/inventario_compra/inventario_compra.php
@@ -27,19 +27,20 @@ if(!JTrama::checkValidStatus($pro)) {
 	$app->redirect($returnUrl, JText::sprintf('JGLOBAL_NO_ACEPTA_COMPRAS', $pro->name), 'error');
 }
 
-
-
 $datosUsuario	= UserData::getUserBalance($idMiddleware->idMiddleware);
 $saldo 			= $datosUsuario->balance == null ? 0: $datosUsuario->balance;
 $uri 			=& JFactory::getURI();
 $callback 		= $uri->toString().'&appId=27';
-
-$action 		= MIDDLE.PUERTO.'/trama-middleware/rest/project/consumeUnits';
-// $action = '/post.php';
-
 $response		= $input->get("response",0,"int"); // RESPUESTA EXITO
 $error			= $input->get("error",0,"int");
 $from			= $input->get("appId",0,"int");
+$confirm		= $input->get("confirm", 0, "int");
+
+if($confirm == 0){
+	$action		= JURI::base().'index.php?option=com_jumi&view=appliction&fileid=27&proyid='.$proyid.'&confirm=1';
+}else{
+	$action		= MIDDLE.PUERTO.'/trama-middleware/rest/project/consumeUnits';
+}
 
 $detalleInversion =  JTrama::getInvestmentDetail($response);
 errorClass::manejoError($error, $from, $proyid);
@@ -57,176 +58,234 @@ jQuery(document).ready(function(){
 });
 </script>
 <?php
-
-if ($error === 0 && $response === 0) {
+if($confirm == 0){
+	if ($error === 0 && $response === 0) {
 ?>
-
-<script>
-
-jQuery(document).ready(function(){
-	function formatoNumero() {
-		jQuery("span.number").number( true, 2 );
-	}
-	jQuery("#form_compra").validationEngine();
-	jQuery('#guardar').prop('disabled', true);
-	function formatoNumero() {
-		jQuery("span.number").number( true, 2 );
-	}
 	
-	function sumaarrecha(){
-		 var precio = 0;
-		 var cant = 0;
-		 var total = 0 ;
-		 jQuery("#form_compra").find("div.wrapper").each(function() {
-			 precio = parseFloat(jQuery(this).find('#precio').val()) || 0;
-			 cant = parseFloat(jQuery(this).find('input.input_compra').val()) || 0;
-			 total += precio * cant;
-			 jQuery("#resultadoglobal").text(total);
-		});			
-			 
-		if ( total > 0 && <?php echo $saldo ?> > total ) {
-			jQuery('#guardar').prop('disabled', false);
-		} else {
-			jQuery('#guardar').prop('disabled', true);
+	<script>
+	
+	jQuery(document).ready(function(){
+		function formatoNumero() {
+			jQuery("span.number").number( true, 2 );
 		}
-
-	}
-	jQuery(':input').change(function(){		
-				
-		var limite = parseInt(jQuery(this).prev().prev().prev().children().text());
-
-		if ( jQuery(this).val()>limite){
-			jQuery(this).val(0);
-			jQuery(this).next().children().text(0);
-			alert('Cantidad superior a inventario');
-
-			sumaarrecha();
-			
-			formatoNumero();
-			//habilitarCompra();
-			
-		}else{
-			var val_uni = parseFloat(jQuery(this).parent().find('#precio').val());
-			var resultado = val_uni * jQuery(this).val();
-
-			jQuery(this).next().children().text(resultado);
-			
-			sumaarrecha();			
-			
-			if ( jQuery(this).val() > 0 ) {
-				var nombre = jQuery(this).attr('id');
-				jQuery(this).attr('name', nombre);
+		jQuery("#form_compra").validationEngine();
+		jQuery('#guardar').prop('disabled', true);
+		function formatoNumero() {
+			jQuery("span.number").number( true, 2 );
+		}
+		
+		function sumaarrecha(){
+			 var precio = 0;
+			 var cant 	= 0;
+			 var total 	= 0 ;
+			 jQuery("#form_compra").find("div.wrapper").each(function() {
+				 precio = parseFloat(jQuery(this).find('#precio').val()) || 0;
+				 cant = parseFloat(jQuery(this).find('input.input_compra').val()) || 0;
+				 total += precio * cant;
+				 jQuery("#resultadoglobal").text(total);
+			});			
+				 
+			if ( total > 0 && <?php echo $saldo ?> > total ) {
+				jQuery('#guardar').prop('disabled', false);
 			} else {
-				jQuery(this).attr('name', '');
+				jQuery('#guardar').prop('disabled', true);
+			}
+	
+		}
+		jQuery(':input').change(function(){		
+					
+			var limite = parseInt(jQuery(this).prev().prev().prev().children().text());
+	
+			if ( jQuery(this).val()>limite){
+				jQuery(this).val(0);
+				jQuery(this).next().children().text(0);
+				alert('Cantidad superior a inventario');
+	
+				sumaarrecha();
+				
+				formatoNumero();
+				//habilitarCompra();
+				
+			}else{
+				var val_uni = parseFloat(jQuery(this).parent().find('#precio').val());
+				var resultado = val_uni * jQuery(this).val();
+	
+				jQuery(this).next().children().text(resultado);
+				
+				sumaarrecha();			
+				
+				if ( jQuery(this).val() > 0 ) {
+					var nombre = jQuery(this).attr('id');
+					jQuery(this).attr('name', nombre);
+				} else {
+					jQuery(this).attr('name', '');
+				}
+				
+				formatoNumero();
+				
+				//habilitarCompra();		
 			}
 			
-			formatoNumero();
-			
-			//habilitarCompra();		
+		});
+		function habilitarCompra() {
+			var total = sumaarrecha();
+				
+			if ( total > 0 && <?php echo $saldo ?> > total ) {
+				jQuery('#guardar').prop('disabled', false);
+			} else {
+				jQuery('#guardar').prop('disabled', true);
+			}
 		}
 		
-	});
-	function habilitarCompra() {
-		var total = sumaarrecha();
+		jQuery('#guardar').click(function() {
 			
-		if ( total > 0 && <?php echo $saldo ?> > total ) {
-			jQuery('#guardar').prop('disabled', false);
-		} else {
-			jQuery('#guardar').prop('disabled', true);
-		}
-	}
-	
-	jQuery('#guardar').click(function() {
-		
-		if(confirm('¿Esta seguro que quiere inverir en esas unidades? ¡Esta accion es IRREVERSIBLE!')) {
-			jQuery("#form_compra").validationEngine();
-			jQuery("#form_compra").submit();
-		} else {
-			alert('Bien hecho');
-		}
+			if(confirm('¿Esta seguro que quiere inverir en esas unidades? ¡Esta accion es IRREVERSIBLE!')) {
+				jQuery("#form_compra").validationEngine();
+				jQuery("#form_compra").submit();
+			} else {
+				alert('Bien hecho');
+			}
+		});
 	});
-});
-</script>
-
-<h1><?php echo JText::_('INVENTARIO_COMPRA');  ?></h1>
-<h2><?php echo $pro->name; ?></h2>
-<div>
-	<form id="form_compra" action="<?php echo $action; ?>" method="POST">
-		<?php
-		$html = '<input type="hidden" name="userId" id="userId" value="'. $idMiddleware->idMiddleware .'" />
-				<input type="hidden" name="projectId" id="projectId" value="'. $pro->id .'" />';
+	</script>
 	
-		foreach ($pro->projectUnitSales as $key => $value){
-
-			$html .= '<div class="wrapper">
-					<div>'.JText::_('LBL_SECCION').': '. $value ->section .'</div>
-					<div>'.JText::_('PRECIO_UNIDAD').': $<span class="number valor_unidad">'. $value ->unitSale.'</span></div>
-					<div>'.JText::_('INVENTARIOPP').': <span>'. $value ->unit .'</span></div>
-					<input id="precio" type="hidden" value="'.$value ->unitSale.'"/>
-					<label>'.JText::_('CANTIDAD_COMPRAR').':</label>
-					<input class="input_compra validate[custom[onlyNumberSp]]" type="number" id="'.$value->id.'" name="" />
-					<div>'.JText::_('TOTAL_SECCION').':$ '.'<span class="number" id="resultados"></span></div>
-					<hr />
+	<h1><?php echo JText::_('INVENTARIO_COMPRA');  ?></h1>
+	<h2><?php echo $pro->name; ?></h2>
+	<div>
+		<form id="form_compra" action="<?php echo $action; ?>" method="POST">
+			<?php
+			$html = '<input type="hidden" name="userId" id="userId" value="'. $idMiddleware->idMiddleware .'" />
+					<input type="hidden" name="projectId" id="projectId" value="'. $pro->id .'" />';
+		
+			foreach ($pro->projectUnitSales as $key => $value){
+	
+				$html .= '<div class="wrapper">
+						<div>'.JText::_('LBL_SECCION').': '. $value ->section .'</div>
+						<div>'.JText::_('PRECIO_UNIDAD').': $<span class="number valor_unidad">'. $value ->unitSale.'</span></div>
+						<div>'.JText::_('INVENTARIOPP').': <span>'. $value ->unit .'</span></div>
+						<input id="precio" type="hidden" value="'.$value ->unitSale.'"/>
+						<label>'.JText::_('CANTIDAD_COMPRAR').':</label>
+						<input class="input_compra validate[custom[onlyNumberSp]]" type="number" id="'.$value->id.'" name="" />
+						<div>'.JText::_('TOTAL_SECCION').':$ '.'<span class="number" id="resultados"></span></div>
+						<hr />
+						</div>';
+			}
+	
+			$html .= '<h3>'. $usuario->name .'</h3>
+					<div>'.JText::_('SALDO_FF').':$ <span class="number">'. $saldo .'</span></div>
+					<div>'.JText::_('TOTAL_PAGAR').':$ <span class="number" id="resultadoglobal"></span></div>
+					<input type="hidden" name="callback" value="'. $callback .'" />
+					<input type="hidden" name="token" value="'. $token .'" />
+					<div style="margin: 10px;">
+						<input type="button" class="button" value="'.JText::_('LBL_CANCELAR').'" onclick="history.go(-1);" />
+						<input type="button" id="guardar" class="button" value="'.JText::_('INVERTIR_PROYECTO').'"  />
+						
 					</div>';
+	
+				echo $html;
+			?>
+		</form>
+		
+	</div>
+	
+	<?php
+	} else {
+		$totalCompra = 0;
+		
+		$html = '<h2>'.$usuario->name.'</h2>
+				<div>'.JText::_('SALDO_FF').': $<span class="number">'. $saldo .'</span></div>
+				<p>'.JText::_('COMPRA_SUCCESS').'</p>
+				<h3>'.JText::_('COMPRA_SUCCESS_DETAILS').'</h3>
+				<div class="detalles_tx">
+					<h3>'.$pro->name.'</h3>
+					<span>'.JText::_('COMPRA_TX_ID').' : </span>'.$response.'
+					<table class="table table-stripped">
+						<tr>
+							<th>'.JText::_('LBL_SECCION').'</th>
+							<th>'.JText::_('PRECIO_UNIDAD').'</th>
+							<th>'.JText::_('CANTIDAD_COMPRADAS').'</th>
+							<th>'.JText::_('COMPRA_SUBTOTAL').'</th>
+						</tr>';
+		foreach ($detalleInversion as $key => $value) {
+	
+			$cant = $value->quantity;
+			$subtotal = $value->unitSale*$cant;
+			$totalCompra = $totalCompra + $subtotal;
+			
+			$html .=	'<tr>
+							<td>'.$value->section.'</td>
+							<td>$<span class="number">'.$value->unitSale.'</span></td>
+							<td>'.$cant.'</td>
+							<td>$<span class="number">'.$subtotal.'</span></td>
+						</tr>';
 		}
-
-		$html .= '<h3>'. $usuario->name .'</h3>
-				<div>'.JText::_('SALDO_FF').':$ <span class="number">'. $saldo .'</span></div>
-				<div>'.JText::_('TOTAL_PAGAR').':$ <span class="number" id="resultadoglobal"></span></div>
-				<input type="hidden" name="callback" value="'. $callback .'" />
-				<input type="hidden" name="token" value="'. $token .'" />
-				<div style="margin: 10px;">
-					<input type="button" class="button" value="'.JText::_('LBL_CANCELAR').'" onclick="history.go(-1);" />
-					<input type="button" id="guardar" class="button" value="'.JText::_('INVERTIR_PROYECTO').'"  />
-					
-				</div>';
-
-			echo $html;
-		?>
-	</form>
-	
-</div>
-
-<?php
-} else {
-	$totalCompra = 0;
-	
-	$html = '<h2>'.$usuario->name.'</h2>
-			<div>'.JText::_('SALDO_FF').': $<span class="number">'. $saldo .'</span></div>
-			<p>'.JText::_('COMPRA_SUCCESS').'</p>
-			<h3>'.JText::_('COMPRA_SUCCESS_DETAILS').'</h3>
-			<div class="detalles_tx">
-				<h3>'.$pro->name.'</h3>
-				<span>'.JText::_('COMPRA_TX_ID').' : </span>'.$response.'
-				<table class="table table-stripped">
-					<tr>
-						<th>'.JText::_('LBL_SECCION').'</th>
-						<th>'.JText::_('PRECIO_UNIDAD').'</th>
-						<th>'.JText::_('CANTIDAD_COMPRADAS').'</th>
-						<th>'.JText::_('COMPRA_SUBTOTAL').'</th>
-					</tr>';
-	foreach ($detalleInversion as $key => $value) {
-
-		$cant = $value->quantity;
-		$subtotal = $value->unitSale*$cant;
-		$totalCompra = $totalCompra + $subtotal;
 		
 		$html .=	'<tr>
-						<td>'.$value->section.'</td>
-						<td>$<span class="number">'.$value->unitSale.'</span></td>
-						<td>'.$cant.'</td>
-						<td>$<span class="number">'.$subtotal.'</span></td>
-					</tr>';
+						<td colspan="3" style="text-align: right;">'.JText::_('TOTAL').'</td>
+						<td><b>$<span class="number">'.$totalCompra.'</span></b></td>
+					</tr>
+					</table>
+					<a class="button" href="index.php?option=com_jumi&view=application&fileid=24&Itemid=218">'.JText::_('ESCRIT').'</a>
+					
+					</div>
+					';
+		echo $html;
 	}
-	
-	$html .=	'<tr>
-					<td colspan="3" style="text-align: right;">'.JText::_('TOTAL').'</td>
-					<td><b>$<span class="number">'.$totalCompra.'</span></b></td>
-				</tr>
-				</table>
-				<a class="button" href="index.php?option=com_jumi&view=application&fileid=24&Itemid=218">'.JText::_('ESCRIT').'</a>
-				
-				</div>
-				';
-	echo $html;
+}else{
+	$params = $input->getArray($_POST);
+	var_dump($params);
+?>
+	<form id="form_compra" action="<?php echo $action; ?>" method="POST">
+		<input type="hidden" name="userId" value="<?php echo $params['userId']; ?>"/>
+		<input type="hidden" name="projectId" value="<?php echo $params['projectId']; ?>"/>
+		<input type="hidden" name="callback" value="<?php echo $params['callback']; ?>"/>
+		<input type="hidden" name="token" value="<?php echo $token; ?>"/>
+		
+		<h2><?php echo JText::_('INVENTARIO_COMPRA_SCREENCONFIRM_TITTLE'); ?></h2>
+		
+		<table class="table table-striped">
+			<tr>
+				<th><?php echo JText::_('LBL_SECCION'); ?></th>
+				<th><?php echo JText::_('PRECIO_UNIDAD'); ?></th>
+				<th><?php echo JText::_('CANTIDAD'); ?></th>
+				<th><?php echo JText::_('TOTAL'); ?></th>
+			</tr>
+			<?php
+				$total = 0;
+				foreach ($pro->projectUnitSales as $key => $value) {
+					foreach ($params as $indice => $valor) {
+						$total = ($value->unitSale*$valor) + $total;
+						
+						if($indice == $value->id){
+			?>
+						<tr>
+							<td><?php echo $value->section; ?></td>
+							<td style="text-align: right;">
+								$<span class="number"><?php echo $value->unitSale; ?></span>
+							</td>
+							<td style="text-align: center;">
+								<?php echo $valor; ?>
+							</td>
+							<td style="text-align: right;">$<span class="number">
+								<?php echo $value->unitSale*$valor; ?></span>
+								<input type="hidden" value="<?php echo $valor; ?>" name="<?php echo $value->id; ?>" />
+							</td>
+						</tr>
+			<?php					
+						}	
+					}
+				} 
+			?>	
+		<tr>
+			<td colspan="3" style="text-align: right;"><?php echo JText::_('TOTAL_PAGAR'); ?></td>
+			<td>$<span class="number"><?php echo $total; ?></span></td>
+		</tr>
+		</table>
+		
+		<div style="margin: 10px;">
+			<input type="button" class="button" value="<?php echo JText::_('LBL_CANCELAR'); ?>" onclick="history.go(-1);" />
+			<input type="submit" id="guardar" class="button" value="<?php echo JText::_('INVERTIR_PROYECTO'); ?>"  />
+		</div>
+	</form>
+<?php
 }
