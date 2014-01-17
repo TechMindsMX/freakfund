56d1e20fc13d61370866646ed5a6e39c24369dca
Ricardo <ricardolyon@gmail.com>
2014-01-17 16:15:43 -0600

    validacion de fecha para aportaciones de capital

--- freakfund/administrator/components/com_aportacionesacapital/views/aportacionesacapital/tmpl/default_body.php
+++ freakfund/administrator/components/com_aportacionesacapital/views/aportacionesacapital/tmpl/default_body.php
@@ -4,12 +4,13 @@ defined('_JEXEC') or die('Restricted Access');
 jimport('trama.usuario_class');
 
 foreach ($this->items as $key => $value) {
-
+var_dump($value->fundEndDate, $value->dateDiff->invert, $value->dateDiff->days);
+	
 ?>
 
 <tr>
 	<td align="absmiddle">
-		<?php if($value->dateDiff->invert == 0 && $value->dateDiff->days <= 300 ) {
+		<?php if($value->dateDiff->invert == 1 && $value->dateDiff->days <= 300 ) {
 			echo '<a href="index.php?option=com_aportacionesacapital&task=detalleproyecto&id='.$value->id.'">'.$value->name.'</a>';
 		} else {
 			echo $value->name;
