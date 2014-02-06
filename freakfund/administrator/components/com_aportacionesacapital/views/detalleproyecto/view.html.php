8042577c2a037c212b11e52ecd04c9833dbc5a85
lutek <lutek.valhalla@gmail.com>
2014-02-06 17:00:16 -0600

    cambios a aportaciones a capital, eliminacion de espacio en blanco

--- freakfund/administrator/components/com_aportacionesacapital/views/detalleproyecto/view.html.php
+++ freakfund/administrator/components/com_aportacionesacapital/views/detalleproyecto/view.html.php
@@ -1,33 +1,23 @@
 <?php
-// No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
-// import Joomla view library
 jimport('joomla.application.component.view');
 
-class detalleProyectoViewdetalleProyecto extends JView
-{
+class detalleProyectoViewdetalleProyecto extends JView{
 	function display($tpl = null) {
-	        // Get data from the model
 	        $items = $this->get('detalleProy');
 
-	        // Check for errors.
 	        if (count($errors = $this->get('Errors'))) {
 	            JError::raiseError(500, implode('<br />', $errors));
 	            return false;
 	        }
 			
-	        // Assign data to the view
 	        $this->items = $items;
-		
-	        // Display the template
 	        $this->addToolBar();
-			
 	        parent::display($tpl);
 	}
+
     protected function addToolBar()	{
         JToolBarHelper::title(JText::_('COM_APORTACIONESCAPITAL_DETALLEPROYECTO_TITLE'));
 		JToolBarHelper::back();
 	}
-
 }
