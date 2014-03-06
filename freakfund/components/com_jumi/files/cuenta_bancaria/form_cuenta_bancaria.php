e2c71ff482bb752a6050ce97e6c60468970f8c31
lutek <lutek.valhalla@gmail.com>
2014-03-06 16:10:27 -0600

    ligar al servicio de la alta de cuenta bancaria

--- freakfund/components/com_jumi/files/cuenta_bancaria/form_cuenta_bancaria.php
+++ freakfund/components/com_jumi/files/cuenta_bancaria/form_cuenta_bancaria.php
@@ -22,6 +22,7 @@ $usuario		= JFactory::getUser();
 $idMiddleware	= UserData::getUserMiddlewareId($usuario->id);
 $datosUsuario	= UserData::getUserBalance($idMiddleware->idMiddleware);
 $callback		= JURI::base().'index.php?option=com_jumi&view=application&fileid=24&from=36';
+$action			= MIDDLE.PUERTO.'/trama-middleware/rest/account/createAccount';
 $confirmacion	= $input->get('confirmacion', 0, 'int');
 $typeAccount	= $input->get('typeAccount', '', 'string');
 $bank			= $input->get('bank', '', 'string');
@@ -31,44 +32,49 @@ $accountNumber	= $input->get('numCuenta', '', 'string');
 
 errorClass::manejoError($error, '36');
 
-if($confirmacion==0){
-	$action	= JURI::base().'index.php?option=com_jumi&view=application&fileid=36';
-	$textBotton = JText::_('LBL_GUARDAR');
-}else{
-	$action	= JURI::base().'index.php?option=com_jumi&view=application&fileid=24';
-	$textBotton = JText::_('LABEL_CONFIRMAR');
-	
-	$tipoCuenta = array("001"=>JText::_('CUENTA_BANCARIA_CUENTA_BANORTE'),
-						"040"=>JText::_('CUENTA_BANCARIA_CLABE'));
+$tipoCuenta = array("001"=>JText::_('CUENTA_BANCARIA_CUENTA_BANORTE'),
+					"040"=>JText::_('CUENTA_BANCARIA_CLABE'));
 
-	
-	$bancos = array("002"=>JText::_('BANAMEX'),
-					"012"=>JText::_('BBVA_BANCOMER'), 
-					"014"=>JText::_('BANCO_SANTANDER'), 
-					"019"=>JText::_('BANJERCITO'), 
-					"021"=>JText::_('HSBC'),
-					"030"=>JText::_('BANCO_BAJIO'),
-					"032"=>JText::_('IXE_BANCO'),
-					"036"=>JText::_('BANCO_INBURSA'),
-					"037"=>JText::_('BANCO_INTERACCIONES'),
-					"042"=>JText::_('BANCA_MIFEL'),
-					"044"=>JText::_('SCOTIABANK_INVERLAT'),
-					"058"=>JText::_('BANCO_REGIONAL DE MONTERREY'),
-					"059"=>JText::_('BANCO_INVEX'),
-					"060"=>JText::_('BANSI'),
-					"062"=>JText::_('BANCA_AFIRME'),
-					"072"=>JText::_('BANORTE'), 
-					"103"=>JText::_('AMERICAN_EXPRESS_BANK'),
-					"106"=>JText::_('BANK_AMERICA_MEXICO'),
-					"127"=>JText::_('BANCO_AZTECA'));
-}
 
+$bancos = array("002"=>JText::_('BANAMEX'),
+				"012"=>JText::_('BBVA_BANCOMER'), 
+				"014"=>JText::_('BANCO_SANTANDER'), 
+				"019"=>JText::_('BANJERCITO'), 
+				"021"=>JText::_('HSBC'),
+				"030"=>JText::_('BANCO_BAJIO'),
+				"032"=>JText::_('IXE_BANCO'),
+				"036"=>JText::_('BANCO_INBURSA'),
+				"037"=>JText::_('BANCO_INTERACCIONES'),
+				"042"=>JText::_('BANCA_MIFEL'),
+				"044"=>JText::_('SCOTIABANK_INVERLAT'),
+				"058"=>JText::_('BANCO_REGIONAL DE MONTERREY'),
+				"059"=>JText::_('BANCO_INVEX'),
+				"060"=>JText::_('BANSI'),
+				"062"=>JText::_('BANCA_AFIRME'),
+				"072"=>JText::_('BANORTE'), 
+				"103"=>JText::_('AMERICAN_EXPRESS_BANK'),
+				"106"=>JText::_('BANK_AMERICA_MEXICO'),
+				"127"=>JText::_('BANCO_AZTECA'));
 ?>
 <script language="JavaScript">
 	jQuery(document).ready(function(){
 		jQuery("#form_cashout").validationEngine();
 		
-		jQuery('.guarda').val('<?php echo $textBotton; ?>');
+		jQuery('.guarda').click(function(){
+			jQuery('.formulario').hide();
+			jQuery('#typeAccount').html(jQuery('#type option:selected').html());
+			jQuery('#bank').html(jQuery('#bankCode option:selected').html());
+			jQuery('#clabeNum').html(jQuery('#account').val());
+			jQuery('.confirmacion').show();
+		});
+		
+		jQuery('.cancelButton').click(function(){
+			jQuery('.formulario').show();
+			jQuery('#type').val('');
+			jQuery('#bankCode').val('');
+			jQuery('#account').val('')
+			jQuery('.confirmacion').hide();
+		});
 		
 		jQuery('#typeAccount').change(function(){
 			var tipoCuenta = jQuery(this).val();
@@ -95,86 +101,73 @@ if($confirmacion==0){
 <div>
 
 	<form id="form_cashout" action="<?php echo $action; ?>" method="POST">
-<?php
-if($confirmacion == 0){
-?>
-		<h1><?php echo JText::_('CUENTA_BANCARIA_TITULO');  ?></h1>
-		
-		<input type="hidden" name="confirmacion" value="1" />
-		
-		<div>
-			<label for="typeAccount"><?php echo JText::_('CUENTA_BANCARIA_TIPO_CUENTA'); ?></label>
-			<select name="typeAccount"  class="validate[required]" id="typeAccount">
-				<option value="001" selected="selected"><?php echo JText::_('CUENTA_BANCARIA_CUENTA_BANORTE'); ?></option>
-				<option value="040"><?php echo JText::_('CUENTA_BANCARIA_CLABE'); ?></option>
-			</select>
+		<div class="formulario">
+			<h1><?php echo JText::_('CUENTA_BANCARIA_TITULO');  ?></h1>
+			
+			<div>
+				<label for="typeAccount"><?php echo JText::_('CUENTA_BANCARIA_TIPO_CUENTA'); ?></label>
+				<select name="type" id="type" class="validate[required]">
+					<?php
+					foreach ($tipoCuenta as $key => $value) {
+						var_dump($key, $value);
+						$selected = $key=='001'?'selected="selected"':'';
+						echo '<option value="'.$key.'" '.$selected.'>'.$value.'</option>';
+					}
+					?>
+				</select>
+			</div>
+			
+			<div>
+				<label for="bank"><?php echo JText::_('CUENTA_BANCARIA_BANCO'); ?></label>
+				<select name="bankCode" id="bankCode"  class="validate[required]">
+					<?php
+					foreach ($bancos as $key => $value) {
+						$selected = $key=='072'?'selected="selected"':'';
+						echo '<option value="'.$key.'" '.$selected.'>'.$value.'</option>';
+					}
+					?>
+				</select>
+			</div>
+			
+			<div>
+				<label for="account"><?php echo JText::_('CUENTA_BANCARIA_CUENTA_CLABE'); ?></label>
+				<input type="text" id="account" name="account" maxlength="16" class="validate[required,custom[onlyNumberSp]]" value="" />
+			</div>
+			
+			<div style="margin: 10px;">
+				<input type="button" class="button cancelButton" value="<?php echo JText::_('LBL_CANCELAR');  ?>">
+				<input type="button" class="button guarda" value="<?php echo JText::_('LBL_GUARDAR'); ?>" />
+			</div>
 		</div>
-		
-		<div>
-			<label for="bank"><?php echo JText::_('CUENTA_BANCARIA_BANCO'); ?></label>
-			<select name="bank" id="bank"  class="validate[required]">
-				<option value="072" selected="selected"><?php echo JText::_('BANORTE') ?></option>
-				<option value="002"><?php echo JText::_('BANAMEX') ?></option>
-				<option value="012"><?php echo JText::_('BBVA_BANCOMER') ?></option> 
-				<option value="014"><?php echo JText::_('BANCO_SANTANDER') ?></option> 
-				<option value="019"><?php echo JText::_('BANJERCITO') ?></option> 
-				<option value="021"><?php echo JText::_('HSBC') ?></option>
-				<option value="030"><?php echo JText::_('BANCO_BAJIO') ?></option>
-				<option value="032"><?php echo JText::_('IXE_BANCO') ?></option>
-				<option value="036"><?php echo JText::_('BANCO_INBURSA') ?></option>
-				<option value="037"><?php echo JText::_('BANCO_INTERACCIONES') ?></option>
-				<option value="042"><?php echo JText::_('BANCA_MIFEL') ?></option>
-				<option value="044"><?php echo JText::_('SCOTIABANK_INVERLAT') ?></option>
-				<option value="058"><?php echo JText::_('BANCO_REGIONAL DE MONTERREY') ?></option>
-				<option value="059"><?php echo JText::_('BANCO_INVEX') ?></option>
-				<option value="060"><?php echo JText::_('BANSI') ?></option>
-				<option value="062"><?php echo JText::_('BANCA_AFIRME') ?></option>
-				<option value="103"><?php echo JText::_('AMERICAN_EXPRESS_BANK') ?></option>
-				<option value="106"><?php echo JText::_('BANK_AMERICA_MEXICO') ?></option>
-				<option value="127"><?php echo JText::_('BANCO_AZTECA') ?></option>
-			</select>
-		</div>
-		
-		<div>
-			<label for="account"><?php echo JText::_('CUENTA_BANCARIA_CUENTA_CLABE'); ?></label>
-			<input type="text" id="account" name="account" maxlength="16" class="validate[required,custom[onlyNumberSp]]" value="<?php echo $accountNumber; ?>" />
-		</div>
-<?php
-}else{
-?>
-		<h1><?php echo JText::_('CUENTA_BANCARIA_TITULO_CONFIRMACION');  ?></h1>
-		<div><?php echo JText::_('CUENTA_BANCARIA_CONFIRMACION'); ?></div>
-				
-		<input type="hidden" name="token" value="<?php echo $token; ?>" />
-		<input type="hidden" name="userId" value="<?php echo $idMiddleware->idMiddleware; ?>" />
-		<input type="hidden" name="userId" value="<?php echo $typeAccount; ?>" />
-		<input type="hidden" name="userId" value="<?php echo $bank; ?>" />
-		<input type="hidden" name="userId" value="<?php echo $account; ?>" />
-		
-		<div>
-			<?php 
-				echo '<div class="div_cuentas">'.JText::_('CUENTA_BANCARIA_TIPO_CUENTA').':</div>'.
-					 '<div class="label_valor div_cuentas"><strong>'.$tipoCuenta[$typeAccount].'</strong></div>'; 
-			?>
-		</div>
-		<div>
-			<?php 
-				echo '<div class="div_cuentas">'.JText::_('CUENTA_BANCARIA_BANCO').':</div>'.
-					 '<div class="label_valor div_cuentas" style="margin-left:9%"><strong>'.$bancos[$bank].'</strong></div>'; 
-			?>
-		</div>
-		<div>
-			<?php 
-				echo '<div class="div_cuentas">'.JText::_('CUENTA_BANCARIA_CUENTA_CLABE').':</div>'.
-					 '<div class="label_valor div_cuentas"><strong>'.$account.'</strong></div>'; 
-			?>
-		</div>
-<?php
-}
-?>
-		<div style="margin: 10px;">
-			<input type="button" class="button" value="<?php echo JText::_('LBL_CANCELAR');  ?>" onClick="btnCancel">
-			<input type="submit" class="button guarda" value="<?php echo JText::_('LBL_GUARDAR'); ?>" />
+
+		<div class="confirmacion" style="display: none;">
+			<h1><?php echo JText::_('CUENTA_BANCARIA_TITULO_CONFIRMACION');  ?></h1>
+			<div><?php echo JText::_('CUENTA_BANCARIA_CONFIRMACION'); ?></div>
+					
+			<input type="hidden" name="token" value="<?php echo $token; ?>" />
+			<input type="hidden" name="userId" value="<?php echo $idMiddleware->idMiddleware; ?>" />
+			<input type="hidden" name="callback" value="<?php echo $callback; ?>" />
+			
+			<div>
+				<?php 
+					echo '<div class="div_cuentas">'.JText::_('CUENTA_BANCARIA_TIPO_CUENTA').':</div> <span class="label_valor" id="typeAccount"></span>';
+				?>
+			</div>
+			<div>
+				<?php 
+					echo '<div class="div_cuentas">'.JText::_('CUENTA_BANCARIA_BANCO').':</div> <span class="label_valor" id="bank"></span>';
+				?>
+			</div>
+			<div>
+				<?php 
+					echo '<div class="div_cuentas">'.JText::_('CUENTA_BANCARIA_CUENTA_CLABE').':</div> <span class="label_valor" id="clabeNum"></span>';
+				?>
+			</div>
+			
+			<div style="margin: 10px;">
+				<input type="button" class="button cancelButton" value="<?php echo JText::_('LBL_CANCELAR');  ?>">
+				<input type="submit" class="button" value="<?php echo JText::_('LBL_GUARDAR'); ?>" />
+			</div>
 		</div>
 	</form>
 
