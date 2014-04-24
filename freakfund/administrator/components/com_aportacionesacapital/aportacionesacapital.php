5895a63c6ace61a4cd018ed863e28e0fdebed82e
lutek <lutek.valhalla@gmail.com>
2014-04-24 14:42:51 -0500

    cambios en la vista por default y al detalle para hacer el traspaso del componente aporte de capital

--- freakfund/administrator/components/com_aportacionesacapital/aportacionesacapital.php
+++ freakfund/administrator/components/com_aportacionesacapital/aportacionesacapital.php
@@ -6,7 +6,7 @@ JFactory::getDocument()->addStyleSheet('components/com_aportacionesacapital/css/
 JTable::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_slave'.DS.'tables');
 
 $jinput = JFactory::getApplication()->input;
-$controllerName = $jinput->get('task', "aportacionesacapital", 'STR' );
+$controllerName = $jinput->get('task', "projectlist", 'STR' );
 
 require_once( JPATH_COMPONENT.DS.'controllers'.DS.$controllerName.'.php' );
 
